024a23c1c6d1bc33695935cdde0d540939e8f1b3
Oscar05cap <odasm.oscar@gmail.com>
2025-05-15 17:04:15 -0600

    Revert "Prolog, GUIFrame, imágenes"
    This reverts commit a3194a85a557509e0c5ed216e18eae7ca873df0d.

--- Nutricion/src/nutricion/MenuDieta.java
+++ Nutricion/src/nutricion/MenuDieta.java
@@ -4,28 +4,20 @@ import java.util.List;
 
 public class MenuDieta {
     private final List<String> desayuno;
-    private final List<String> almuerzo;
     private final List<String> comida;
     private final List<String> merienda;
-    private final List<String> cena;
     private final int kcal;
 
-    public MenuDieta(List<String> desayuno, List<String> comida, List<String> merienda, List<String> almuerzo, List<String> cena, int kcal) {
+    public MenuDieta(List<String> desayuno, List<String> comida, List<String> merienda, int kcal) {
         this.desayuno = desayuno;
-        this.almuerzo = almuerzo;
         this.comida = comida;
         this.merienda = merienda;
-        this.cena = cena;
         this.kcal = kcal;
     }
 
     public List<String> getDesayuno()
     {
         return desayuno;
-    }
-
-    public List<String> getAlmuerzo() {
-        return almuerzo;
     }
 
     public List<String> getComida()
@@ -37,17 +29,12 @@ public class MenuDieta {
     {
         return merienda;
     }
-
-    public List <String> getCena() {
-        return cena;
-    }
+    
     @Override
     public String toString() {
         return "Desayuno: " + desayuno + "\n" +
-                "Almuerzo: " + almuerzo + "\n" +
                "Comida: " + comida + "\n" +
                "Merienda: " + merienda + "\n" +
-                "Cena: " + cena + "\n" +
                "Total Kcalorías: " + kcal;
     }
 }

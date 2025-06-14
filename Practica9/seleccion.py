b6c88d7fe400c18c6111c8bd41979dfb69488465
Uriel <evelazqueza1900@alumno.ipn.mx>
2025-06-14 11:03:22 -0600

    Practica 9

--- Practica9/seleccion.py
+++ Practica9/seleccion.py
@@ -1,4 +1,4 @@
-#Importamos todas las librerias para poder 
+# Importamos todas las librerias para poder ejecutar el codigo 
 import joblib
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,19 +7,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix, ConfusionMatrixDisplay
 
+
 archivo = input('Dime el nombre del archivo CSV: ')
 
-file_name = archivo  + ".csv"
+file_name = archivo + ".csv"
 
-dataframe  = pd.read_csv(file_name)
+dataframe = pd.read_csv(file_name)
 
-x = dataframe.iloc[:,:-1]
+X = dataframe.iloc[:, :-1]
 
 y = dataframe.iloc[:, -1]
 
 maximo = X.shape[1]
 
-modelo = RandomForestClassifier(random_state=42) 
+modelo = RandomForestClassifier(random_state=42)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -31,31 +32,85 @@ score = []
 values = []
 n_features = 0
 
-roc = 0 
+roc = 0
 
-for i in range (2, maximo + 1):
-	selector_caracteristicas = RFE(modelo, n_features_to_select = i, step  =1)
+for i in range(2, maximo + 1):
+    selector_caracteristicas = RFE(modelo, n_features_to_select=i, step=1)
 
-	selector_caracteristicas = selector_caracteristicas.fit(X_train, y_train)
+    selector_caracteristicas = selector_caracteristicas.fit(X_train, y_train)
 
-	selected_features = X.columns[selector_caracteristicas.support_]
+    selected_features = X.columns[selector_caracteristicas.support_]
 
-	 X_new = dataframe.loc[:, selected_features]
+    X_new = dataframe.loc[:, selected_features]
 
-	 X_ntrain, X_ntest, y_train, y_test = train_test_split(X_new, y, test_size = 0.2, random_state= 42)
+    X_ntrain, X_ntest, y_train, y_test = train_test_split(X_new, y, test_size=0.2, random_state=42)
 
-	 modelo.fit(X_ntrain,y_train)
+    modelo.fit(X_ntrain, y_train)
 
-	 y_pred = modelo.predict(X_ntest)
+    y_pred = modelo.predict(X_ntest)
 
-	 roc_auc = roc_auc_score(y_test, y_pred)
+    roc_auc = roc_auc_score(y_test, y_pred)
 
-	 scores.append(roc_auc)
-	 values.append(i)
+    score.append(roc_auc)
+    values.append(i)
+
+    if roc_auc > roc:
+        roc = roc_auc
+        best_features = selected_features
+        n_features = i
 
 
-tittle = f"Seleccion de características usando RFE ({file_name})"
-plt.figure(figsize = (10,6))
-plt.plot(values,scores,marker = 'o')
-plt.xlabel('Numero de características seleccionadas')
+title = f"Selección de características usando RFE ({file_name})"
+plt.figure(figsize=(10, 6))
+plt.plot(values, score, marker='o')
+plt.xlabel('Número de características seleccionadas')
 plt.ylabel('ROC AUC')
+plt.title(title)
+plt.grid(True)
+plt.show()        
+
+
+print("\nMejor ROC AUC: ", roc)
+message = f"\nMuestra las {n_features} características seleccionadas: \n"
+print(message)
+for feature in best_features:
+    print(feature )
+
+X_best = dataframe.loc[:, best_features]
+
+X_btrain, X_btest, y_train, y_test = train_test_split(X_best, y, test_size=0.2, random_state=42)
+
+modelo.fit(X_btrain,y_train)
+
+y_pred = modelo.predict(X_btest)
+
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+roc_auc = roc_auc_score(y_test, y_pred)
+
+
+
+print("\nMetricas de rendimiento: \n")
+print("Exactitud (Accuracy): ")
+print("Precisión (Precision):  ")
+print("Sensibilidad (Recall): ")
+print("Puntuación F1 (F1 score): ")
+print("ROC AUC: ", roc_auc)
+
+print("Matriz de confusión: ")
+disp = ConfusionMatrixDisplay(confusion_matrix=conf_matrix)
+disp.plot(cmap=plt.cm.PuBuGn)
+plt.show()
+
+
+
+model_and_features = {
+    'model': modelo,
+    'feature_names': best_features
+}
+
+output_file_name = f'{archivo}cancer_random_forest_model.joblib'
+joblib.dump(model_and_features, output_file_name)
